aae054a1ea0129afd81aa1e13149a8dc0dc91f33
LibbyCA <146595630+LibbyCA@users.noreply.github.com>
2024-02-29 01:06:10 +0000

    finish_skeleton_code

--- coursework/mazegame.c
+++ coursework/mazegame.c
@@ -6,6 +6,7 @@ typedef struct{
     int y; //help to traverse maze array
     int start; //S on maze
     int player; //current user position
+    char input; //WASD and M options entered by user
 }
 
 int maxSize = 100; //greatest number of rows/columns maze can have
@@ -39,14 +40,35 @@ void map_maze (){
     // if so shows player's current position so prints an "X"
     // else prints maze[i][j]
     // this should print an image of the map of the maze with the players current position
-    
+
 }
-int main(){
+int main(int argc, char *argv[]){ //declare arguments
+    
+    // validation check to ensure no more or less than two arguments are declared by user - argc != 2
+    // return 1 (error message) if so
+    // argv[1] is the maze file name entered by user
 
-    // load maze
-    // find height and width of maze using a for loop
+    //use load_maze function above to load argv[1] 
 
-    //go through maze file and print each element individually
+    //use a while(1) loop so the loop repeats forever until a break statement is found
+    // use a switch loop to complete the WASD and M options
+    // print the options the user has
+    // scanf for a char 
+    // case 'W' check if the coordinate above is '#' (wall) - print message telling the player they cannot move in this direction
+    // otherwise 
+    // case 'A' check is coordinate to the left is '#' (wall) - print message telling the player they cannot move in this direction
+    // otherwise
+    //case 'S' check is coordinate below is '#' (wall) - print message telling the player they cannot move in this direction
+    // otherwise
+    // case 'D' check is coordinate to the right is '#' (wall) - print message telling the player they cannot move in this direction
+    // otherwise
+    // case 'M' call on the map_maze function above
+    // default - error message - shouldn't appear as test script should ensure no other characters are entered
+
+    // outside of the switch loop but still in the while loop - check if the player current position is E for the end
+    // if print a congratulatory message and break out of the loop
+
+    
 
     //switch loop
     // w - check if wall above else increment y position
